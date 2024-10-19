e75db53b8a34f90f48cb90dcf030b83a12a013e4
prashanthPadmasahli <prashanthkumarshettigar@gmail.com>
2024-10-19 19:09:41 +0530

    Initial Commit

---
+++ ExapandableList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExapandableList
+//
+//  Created by mac on 19/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExapandableList/ExapandableListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExapandableListApp.swift
+//  ExapandableList
+//
+//  Created by mac on 19/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ExapandableListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
